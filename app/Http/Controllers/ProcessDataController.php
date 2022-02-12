1397fc28f7d782a23999922394a9c51afc8f41f1
amal <amalramachandran8@gmail.com>
2022-02-12 22:36:11 +0530

    some change

--- app/Http/Controllers/ProcessDataController.php
+++ app/Http/Controllers/ProcessDataController.php
@@ -41,22 +41,7 @@ class ProcessDataController extends Controller
        
        } else{
 
-// $min_date =  loan_detail::all('first_payment_date')->min('first_payment_date');
-// $max_date =  loan_detail::all('last_payment_date')->max('last_payment_date');
-// $start    = new DateTime($min_date);
-// $start->modify('first day of this month');
-// $end      = new DateTime($max_date);
-// $end->modify('first day of next month');
-// $interval = DateInterval::createFromDateString('1 month');
-// $period   = new DatePeriod($start, $interval, $end);
-
-// foreach ($period as $dt) {
-//     echo $dt->format("Y-M"). '<br>';
-// }
-
-
-            Schema::create('emi_details', function($table)
-                {
+            Schema::create('emi_details', function($table){
                  $table->id();
                  $table->string('clientid');
                 
